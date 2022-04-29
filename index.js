90bdf57fcc856313824cc20ca420e058bf5294b4
Levi Hutchinson Burns <leburns62@gmail.com>
2022-04-29 16:35:50 -0400

    commit tree 0e0ee27ab7f210cfd525d497b6e4483691bf9eaf

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(action = 'roller-skate') {
+    return `This Saturday, I want to ${action}!`;
+}
+
+function mondayWork(work = 'go to the office') {
+    return `This Monday, I will ${work}.`
+}
+
+function wrapAdjective(flair = "*") {
+    return function (adj = "special") {
+        return `You are ${flair}${adj}${flair}!`;
+    }
+}
